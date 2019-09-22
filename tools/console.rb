de9156c473851730f3bcf26f716fd3bc97128856
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-22 02:06:35 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -10,6 +10,7 @@ end
 lorde = Cult.new("Cult Lorde", "NY", 1990, "Be Royal")
 jagger = Cult.new("Stones Cult", "LA", 1980, "Something with a stone")
 bowie = Cult.new("Cult of Oddity", "Space", 1970, "Where you at, Tom")
+nicks = Cult.new("Fleetwood", "LA", 1994, "Rhiannon")
 
 steve = Follower.new("Steve", 25, "Sup Im steve")
 anton = Follower.new("Anton", 21, "anton is here")
@@ -20,6 +21,8 @@ s_jagger = steve.join_cult(jagger)
 a_bowie = anton.join_cult(bowie)
 a_lorde = anton.join_cult(lorde)
 j_bowie = jill.join_cult(bowie)
+j_nicks = jill.join_cult(nicks)
+
 
 binding.pry
 
