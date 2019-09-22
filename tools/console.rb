3223adbe3a6290d95e8bde7cb2d9f51a2a07c91a
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-22 01:20:40 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+lorde = Cult.new("Cult Lorde", "NY", 1990, "Be Royal")
+jagger = Cult.new("Stones Cult", "LA", 1980, "Something with a stone")
+bowie = Cult.new("Cult of Oddity", "Space", 1970, "Where you at, Tom")
+
+steve = Follower.new("Steve", 25, "Sup Im steve")
+anton = Follower.new("Anton", 21, "anton is here")
+jill = Follower.new("Jill", 33, "Hey yall")
+
+oath1 = jill.join_cult(lorde)
+oath2 = steve.join_cult(jagger)
+oath3 = anton.join_cult(bowie)
+oath4 = anton.join_cult(lorde)
+oath5 = jill.join_cult(bowie)
+
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
