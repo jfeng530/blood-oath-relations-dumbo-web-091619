9e7c0e63e563e1d4ce06b6a369cffd8bc96f07af
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-22 01:40:05 +0000

    Automatically backed up by Learn

--- app/models/Cult.rb
+++ app/models/Cult.rb
@@ -1,67 +1,72 @@
 require 'date'
 class Cult
 
-    attr_accessor :name, :location, :founding_year, :slogan, :population
+    attr_accessor :name, :location, :founding_year, :slogan
     @@all = []
 
     def initialize(name, location, founding_year, slogan)
-        @name = name
-        @location = location
-        @founding_year = founding_year
-        @slogan = slogan
-        @population = self.oaths.count
-        @@all << self
+      @name = name
+      @location = location
+      @founding_year = founding_year
+      @slogan = slogan
+      @@all << self
     end
 
     def self.all
-        @@all
+      @@all
     end
 
     def recruit_follower(person)
-        BloodOath.new(self, person, Date.today.to_s)
+      BloodOath.new(self, person, Date.today.to_s)
     end
 
     def cult_population
-        @population = self.oaths.count
-        @population
+      @population = self.oaths.count
+      @population
     end
 
     def oaths
-        BloodOath.all.select {|oath| oath.cult == self}.uniq
+      BloodOath.all.select {|oath| oath.cult == self}.uniq
     end
 
     def members
-        self.oaths.map {|oath| oath.follower}.uniq
+      self.oaths.map {|oath| oath.follower}.uniq
     end
 
     def self.find_by_name(name)
-        Cult.all.find {|cult| cult.name == name}
+      Cult.all.find {|cult| cult.name == name}
     end
 
     def self.find_by_location(location)
-        Cult.all.select {|cult| cult.location == location}
+      Cult.all.select {|cult| cult.location == location}
     end
 
     def self.find_by_founding_year(year)
-        Cult.all.select {|cult| cult.founding_year == year}
+      Cult.all.select {|cult| cult.founding_year == year}
     end
 
     def average_age
-        total_age = 0
-        self.members.each do |member|
-            total_age += member.age 
-        end
-        total_age/self.members.count
+      total_age = 0
+      self.members.each do |member|
+          total_age += member.age 
+      end
+      total_age/self.members.count
     end
 
     def my_followers_mottos
-        self.members.map do |member|
-            member.life_motto
-        end
+      self.members.map do |member|
+          member.life_motto
+      end
     end
 
     def self.least_popular
-   
+      pop = 0
+      bad_cult = nil
+      Cult.all.each do |cult|
+        if cult.members.count > pop
+          pop = cult.members.count
+          bad_cult = self
+      end
     end
 
 
