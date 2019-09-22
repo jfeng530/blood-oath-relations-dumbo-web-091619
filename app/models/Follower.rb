4b8c59751895de49c40f9aa6a713ac3c3b969fec
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-22 01:21:10 +0000

    Automatically backed up by Learn

---
+++ app/models/Follower.rb
@@ -0,0 +1,32 @@
+class Follower
+
+    attr_accessor :name, :age, :life_motto
+    @@all = []
+
+    def initialize(name, age, life_motto)
+        @name = name
+        @age = age
+        @life_motto = life_motto
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def oaths_made
+        BloodOath.all.select {|oath| oath.follower == self}.uniq
+    end
+
+    def cults
+        self.oaths_made.map {|oath| oath.cult}.uniq
+    end
+
+    def join_cult(cult)
+        cult.recruit_follower(self)
+    end
+
+    def self.of_a_certain_age(age)
+        self.all.select {|follower| follower.age >= age}
+    end
+end
