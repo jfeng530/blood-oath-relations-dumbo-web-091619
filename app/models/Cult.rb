4b2c95699d4611e5d42f622e6b28048ab36e7d9f
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-22 03:02:17 +0000

    Automatically backed up by Learn

--- app/models/Cult.rb
+++ app/models/Cult.rb
@@ -1,14 +1,15 @@
 require 'date'
 class Cult
 
-    attr_accessor :name, :location, :founding_year, :slogan
+    attr_accessor :name, :location, :founding_year, :slogan, :minimum_age
     @@all = []
 
-    def initialize(name, location, founding_year, slogan)
+    def initialize(name, location, founding_year, slogan, minimum_age)
       @name = name
       @location = location
       @founding_year = founding_year
       @slogan = slogan
+      @minimum_age = minimum_age
       @@all << self
     end
 
