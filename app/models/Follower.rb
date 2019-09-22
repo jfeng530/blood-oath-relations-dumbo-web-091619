a62193077f2e94f0f243553c8bd84a37b63b7a64
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-22 02:20:06 +0000

    Automatically backed up by Learn

--- app/models/Follower.rb
+++ app/models/Follower.rb
@@ -30,9 +30,9 @@ class Follower
         self.all.select {|follower| follower.age >= age}
     end
     
-    def my_cults_mottos
+    def my_cults_slogans
       self.cults.map do |cult|
-          cult.motto 
+          cult.slogan
       end
     end
     
