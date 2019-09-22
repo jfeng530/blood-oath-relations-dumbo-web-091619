f26afac13c1bc2438e10922dc0066f8b6226c90c
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-22 01:21:00 +0000

    Automatically backed up by Learn

---
+++ app/models/Cult.rb
@@ -0,0 +1,66 @@
+require 'date'
+class Cult
+
+    attr_accessor :name, :location, :founding_year, :slogan
+    @@all = []
+
+    def initialize(name, location, founding_year, slogan)
+        @name = name
+        @location = location
+        @founding_year = founding_year
+        @slogan = slogan
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def recruit_follower(person)
+        BloodOath.new(self, person, Date.today.to_s)
+    end
+
+    def cult_population
+        self.oaths.count
+    end
+
+    def oaths
+        BloodOath.all.select {|oath| oath.cult == self}.uniq
+    end
+
+    def members
+        self.oaths.map {|oath| oath.follower}.uniq
+    end
+
+    def self.find_by_name(name)
+        Cult.all.find {|cult| cult.name == name}
+    end
+
+    def self.find_by_location(location)
+        Cult.all.select {|cult| cult.location == location}
+    end
+
+    def self.find_by_founding_year(year)
+        Cult.all.select {|cult| cult.founding_year == year}
+    end
+
+    def average_age
+        total_age = 0
+        self.members.each do |member|
+            total_age += member.age 
+        end
+        total_age/self.members.count
+    end
+
+    def my_followers_mottos
+        self.members.map do |member|
+            member.life_motto
+        end
+    end
+
+    def self.least_popular
+        
+    end
+
+
+end
